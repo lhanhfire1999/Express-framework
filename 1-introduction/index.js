c11aa5ad2730e6e27d1610b99e2f23d2c985b922
AnhLe <lhanhfire1999@gmail.com>
2020-09-21 12:37:38 +0700

    template-engines/example

--- 1-introduction/index.js
+++ 1-introduction/index.js
@@ -2,14 +2,27 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('/', function(request,respone){
-	respone.send('<h1> Hello Coders.Tokyo <h1>');
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+app.get('/', function(req,res){
+	res.render('index',{
+		name: 'Anh'
+	});
 });
 
-app.get('/users',function(request,respone){
-	respone.send('User list');
+app.get('/users',function(req,res){
+	res.render('users/index',{
+		users:[
+		{id:1 , name:'Anh'},
+		{id:2 , name:'Anh3'},
+		{id:3 , name:'Anh2'}
+		]
+	});
 });
 
 app.listen(port,function(){
 	console.log('Server listening on port', port);
 });
+
