3afa4688ce62634efa8485c0a00091dca49ef2c8
AnhLe <lhanhfire1999@gmail.com>
2020-09-24 14:01:18 +0700

    view-user

--- 1-introduction/index.js
+++ 1-introduction/index.js
@@ -2,19 +2,17 @@
 var express = require('express');
 var app = express();
 var port = 3000;
-
+//shortID for database-setup
+var shortid = require('shortid');
 //req-body setup
-var bodyParser = require('body-parser')
-
+var bodyParser = require('body-parser');
 //Database setup
 var low = require('lowdb');
 var FileSync = require('lowdb/adapters/FileSync');
 var adapter = new FileSync('db.json');
 var db = low(adapter);
 
-db.defaults({ users: [] })
-  .write();
-
+db.defaults({ users: [] }).write();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -22,7 +20,7 @@ app.set('views', './views');
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-
+//GET Method
 app.get('/', function(req,res){
 	res.render('index',{
 		name: 'Anh'
@@ -50,7 +48,20 @@ app.get('/users/search',function(req,res){
 app.get('/users/create',function(req,res){
 	res.render('users/create.pug');
 });
+
+app.get('/users/:id',function(req,res){
+	var id = req.params.id;
+	var user = db.get('users').find({ id : id}).value();
+	console.log(user);
+	res.render('users/view',{
+		user:user
+	});
+
+});
+
+//POST Method
 app.post('/users/create',function(req,res){
+	req.body.id = shortid.generate();
 	db.get('users').push(req.body).write();
 	res.redirect('/users');
 });
