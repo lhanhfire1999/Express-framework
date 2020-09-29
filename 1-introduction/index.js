1e44d8e5ec7f60032d4cb5b253b3e19fc12aa5de
AnhLe <lhanhfire1999@gmail.com>
2020-09-29 12:31:38 +0700

    static-file

--- 1-introduction/index.js
+++ 1-introduction/index.js
@@ -11,6 +11,8 @@ app.set('views', './views');
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+app.use(express.static('public'));
+
 app.get('/', function(req,res){
 	res.render('index',{
 		name: 'Anh'
@@ -18,6 +20,7 @@ app.get('/', function(req,res){
 });
 
 app.use('/users',userRoute);
+
 
 app.listen(port,function(){
 	console.log('Server listening on port', port);
