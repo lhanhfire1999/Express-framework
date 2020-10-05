713b16bbfd166bdee6b4c77a2606b9e7157a2826
AnhLe <lhanhfire1999@gmail.com>
2020-10-05 22:32:13 +0700

    signed-cookies

--- 1-introduction/index.js
+++ 1-introduction/index.js
@@ -15,7 +15,7 @@ app.set('views', './views');
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(cookieParser());
+app.use(cookieParser('asdasdsad123sa'));
 
 app.use(express.static('public'));
 
